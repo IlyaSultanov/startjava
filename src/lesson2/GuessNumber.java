7cbf3499d3b69a359970ccc1e14da739f791482c
sultanov <i.sultanov@corp.rtln.ru>
2019-12-01 11:25:20 +0300

    BaseJava lesson 2

--- src/lesson2/GuessNumber.java
+++ src/lesson2/GuessNumber.java
@@ -1,11 +1,8 @@
 public class GuessNumber {
 
     private Scanner scanner = new Scanner(System.in);
-
-
     private Player player1;
     private Player player2;
-    private boolean isPlayerGuess;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
@@ -15,34 +12,27 @@ public class GuessNumber {
     public void start() {
         int mysteryNumber = (int) (Math.random() * 101);
         while (true) {
-            inputNumber(player1);
-            compare(player1, mysteryNumber);
-            if (!isPlayerGuess) {
+            if (makeMove(player1, mysteryNumber)) {
                 return;
             }
-            inputNumber(player2);
-            compare(player2, mysteryNumber);
-            if (!isPlayerGuess) {
+            if (makeMove(player2, mysteryNumber)) {
                 return;
             }
         }
     }
 
-    private void inputNumber(Player player) {
+    private boolean makeMove(Player player, int mysteryNumber) {
         System.out.println("Игрок " + player.getName() + ", введите число");
         player.setNumber(scanner.nextInt());
-    }
-
-    private void compare(Player player, int mysteryNumber) {
         if (player.getNumber() > mysteryNumber) {
             System.out.println("Число слишком большое");
-            isPlayerGuess = true;
+            return false;
         } else if (player.getNumber() < mysteryNumber) {
             System.out.println("Число слишком маленькое");
-            isPlayerGuess = true;
+            return false;
         } else {
             System.out.println(player.getName() + ", вы угадали!");
-            isPlayerGuess = false;
+            return true;
         }
     }
 }
--- src/lesson2/GuessNumberTest.java
+++ src/lesson2/GuessNumberTest.java
@@ -1,7 +1,6 @@
 public class GuessNumberTest {
 
     private static Scanner scanner = new Scanner(System.in);
-    private static boolean indicator = true;
 
     public static void main(String[] args) {
         Player player1 = new Player(inputName(1));
@@ -9,8 +8,7 @@ public class GuessNumberTest {
         GuessNumber guessNumber = new GuessNumber(player1, player2);
         do {
             guessNumber.start();
-            continueAsk();
-        } while (indicator);
+        } while (isContinue());
         System.out.println("Завершение программы");
     }
 
@@ -19,15 +17,16 @@ public class GuessNumberTest {
         return scanner.next();
     }
 
-    private static void continueAsk() {
+    private static boolean isContinue() {
         String playerAnswer;
         do {
             System.out.println("Хотите продолжить? (да/нет)");
             playerAnswer = scanner.next();
-            if (playerAnswer.equals("нет")) {
-                indicator = false;
+            if (playerAnswer.equals("да")) {
+                return true;
             }
         }
-        while (!playerAnswer.equals("да") && !playerAnswer.equals("нет"));
+        while (!playerAnswer.equals("нет"));
+        return false;
     }
 }
