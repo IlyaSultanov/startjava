b7dce14c5e19af624bdfd1afb6920aa502b75b64
sultanov <i.sultanov@corp.rtln.ru>
2019-12-02 10:43:40 +0300

    BaseJava lesson 2

--- src/lesson2/GuessNumber.java
+++ src/lesson2/GuessNumber.java
@@ -3,6 +3,7 @@ public class GuessNumber {
     private Scanner scanner = new Scanner(System.in);
     private Player player1;
     private Player player2;
+    private int mysteryNumber;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
@@ -10,7 +11,7 @@ public class GuessNumber {
     }
 
     public void start() {
-        int mysteryNumber = (int) (Math.random() * 101);
+        mysteryNumber = (int) (Math.random() * 101);
         while (true) {
             if (makeMove(player1, mysteryNumber)) {
                 return;
@@ -22,17 +23,20 @@ public class GuessNumber {
     }
 
     private boolean makeMove(Player player, int mysteryNumber) {
-        System.out.println("Игрок " + player.getName() + ", введите число");
-        player.setNumber(scanner.nextInt());
+        inputNumber(player);
         if (player.getNumber() > mysteryNumber) {
             System.out.println("Число слишком большое");
-            return false;
         } else if (player.getNumber() < mysteryNumber) {
             System.out.println("Число слишком маленькое");
-            return false;
         } else {
             System.out.println(player.getName() + ", вы угадали!");
             return true;
         }
+        return false;
+    }
+
+    private void inputNumber(Player player) {
+        System.out.println("Игрок " + player.getName() + ", введите число");
+        player.setNumber(scanner.nextInt());
     }
 }
